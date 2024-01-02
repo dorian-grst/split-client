f64cdbc3b01dadba1d87c29dd5448980ff9d9b8b
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-01-02 16:46:45 +0100

    update files

--- src/components/modal/BasicModal.tsx
+++ src/components/modal/BasicModal.tsx
@@ -6,6 +6,7 @@ import InputGroupModal from './InputGroupModal';
 interface ModalProps {
     isOpen: boolean;
     setIsOpen: (value: boolean) => void;
+    value?: string;
     title: string;
     titleClass?: string;
     label: string;
@@ -14,19 +15,33 @@ interface ModalProps {
     textRightButton: string;
     onClickLeftButton?: () => void;
     onClickRightButton?: () => void;
+    onInputChange?: (value: string) => void;
 }
 
-export default function BasicModal({ isOpen, setIsOpen, title, titleClass, label, placeholder, textLeftButton, textRightButton, onClickLeftButton, onClickRightButton }: ModalProps) {
+export default function BasicModal({
+    isOpen,
+    setIsOpen,
+    value,
+    title,
+    titleClass,
+    label,
+    placeholder,
+    textLeftButton,
+    textRightButton,
+    onClickLeftButton,
+    onClickRightButton,
+    onInputChange,
+}: ModalProps) {
     const completeButtonRef = useRef(null);
 
     return (
         <Dialog initialFocus={completeButtonRef} open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50 w-min">
-            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+            <div className="fixed inset-0 bg-gray-950/30" aria-hidden="true" />
             <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
                 <Dialog.Panel className="rounded-lg border border-light-gray bg-white">
                     <div className="flex flex-col gap-[10px] border-b border-light-gray p-8">
                         <h1 className={titleClass}>{title}</h1>
-                        <InputGroupModal label={label} placeholder={placeholder} />
+                        <InputGroupModal label={label} placeholder={placeholder} value={value} onChange={(value) => (onInputChange ? onInputChange(value) : undefined)} />
                     </div>
                     <div className="flex flex-row justify-between p-8">
                         <button onClick={onClickLeftButton} className="cancel">
