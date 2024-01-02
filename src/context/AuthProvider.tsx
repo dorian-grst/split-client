38e5013aaeba1b2cd871995e3535e2ccf58bc4cb
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-01-02 16:46:01 +0100

    update components & providers

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,15 +1,58 @@
-import { PropsWithChildren, createContext, useState } from 'react';
+import axios from 'axios';
+import { PropsWithChildren, createContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+const VITE_API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 
 interface User {
     id: string;
     email: string;
-    splits: [];
+    display_name: string;
 }
 
 export const defaultUser: User = {
     id: '',
     email: '',
-    splits: [],
+    display_name: '',
+};
+
+export const updateDisplayName = async (newDisplayName: string, userId: string) => {
+    try {
+        const response = await axios.patch(
+            VITE_API_ENDPOINT + '/v1/user/' + userId,
+            {
+                displayName: newDisplayName,
+            },
+            { withCredentials: true }
+        );
+        toast.success('Display name updated!');
+        console.log(response.data);
+    } catch (error) {
+        toast.error('An error occured while updating your display name.');
+        console.error(error);
+    }
+};
+
+export async function getUserInfos(): Promise<{
+    id: string;
+    email: string;
+    display_name: string;
+}> {
+    const user = await axios.get(VITE_API_ENDPOINT + '/v1/auth/me', {
+        withCredentials: true,
+    });
+    return await user.data;
+}
+
+export const getAllUserSplits = async (userId: string) => {
+    try {
+        const response = await axios.get(VITE_API_ENDPOINT + '/v1/user/' + userId + '/splits', {
+            withCredentials: true,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export const AuthContext = createContext<{
@@ -22,6 +65,14 @@ export const AuthContext = createContext<{
 
 export default function AuthProvider({ children }: PropsWithChildren<{}>) {
     const [user, setUser] = useState<User>(defaultUser);
+
+    useEffect(() => {
+        async function fetchUserInfos() {
+            const userInfos = await getUserInfos();
+            setUser(userInfos);
+        }
+        fetchUserInfos();
+    }, []);
 
     return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 }
