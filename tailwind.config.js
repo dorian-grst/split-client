8c39a3cf2e7f3e33b716d64f4c0689d25cb96ec8
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-01-02 16:46:15 +0100

    modify config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,13 @@ export default {
     theme: {
         extend: {
             colors: {
-                red: '#ff5151',
                 'red-bis': '#ffeaea',
                 'green-primary': '#34a25b',
                 'green-secondary': '#8ba04d',
                 'purple-primary': '#98287d',
                 'purple-secondary': '#3054a1',
-                white: '#ffffff',
                 'light-gray': '#e6e6e6',
                 'gray-bis': '#e9edf3',
-                gray: '#6c6c6c',
-                black: '#24292e',
                 'black-second': '#393E42',
                 'dark-black': '#000000',
                 discord: '#5865f2',
