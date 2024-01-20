8638a37ba33f60d798d103127b98a2b98b33cbad
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-01-20 19:21:27 +0100

    correct ref warn

--- src/components/dropdown/AlertDropdown.tsx
+++ src/components/dropdown/AlertDropdown.tsx
@@ -8,13 +8,13 @@ interface ButtonMenuProps {
     textClasses?: string;
 }
 
-const AlertDropdown = React.forwardRef<HTMLButtonElement, ButtonMenuProps>(({ text, onClick, additionalClasses, textClasses }) => {
+const AlertDropdown = React.forwardRef<HTMLDivElement, ButtonMenuProps>(({ text, onClick, additionalClasses, textClasses }, ref) => {
     const buttonClasses = `flex w-full cursor-default flex-row items-center justify-between font-medium rounded-lg px-3 py-2 hover:bg-slate-100 ${additionalClasses || ''}`;
 
     return (
-        <div className={buttonClasses}>
+        <div className={buttonClasses} ref={ref}>
             <div className="flex flex-row items-center justify-center gap-5">
-                <h3 className={`text-left text-gray-950 ${textClasses}`}>{text}</h3>
+                <h3 className={`text-left text-gray-950 flex-wrap ${textClasses}`}>{text}</h3>
                 <div className="flex flex-row items-center justify-center gap-2">
                     <button className="cancel border-purple-primary px-2 py-1 text-purple-primary" onClick={onClick}>
                         <TrashIcon className="h-4 w-4" />
